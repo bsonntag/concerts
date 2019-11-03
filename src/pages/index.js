fe86d15de25989614d3576d50f5bccad39e130c6
Benjamim Sonntag <benjamimsonntag@gmail.com>
2019-11-03 20:33:48 +0000

    Add home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,59 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import styled from 'styled-components';
 import { Metatags } from '../components/metatags';
+import { PageLayout } from '../components/page-layout';
+
+const query = graphql`
+  query {
+    allContentfulConcert(sort: { fields: date, order: DESC }) {
+      nodes {
+        id
+        name
+        date
+        bands
+        location
+      }
+    }
+  }
+`;
+
+const ListItem = styled.li`
+  &::before {
+    content: '';
+    background-color: white;
+    display: block;
+    height: 1px;
+    width: 100%;
+    margin: var(--medium-space) 0;
+  }
+`;
 
 function HomePage() {
+  const data = useStaticQuery(query);
+
   return (
-    <main>
+    <PageLayout>
       <Metatags />
-      <h1>Hi people</h1>
-    </main>
+      <h1>Concerts</h1>
+
+      <ol>
+        {data.allContentfulConcert.nodes.map(concert => (
+          <ListItem key={concert.id}>
+            <h2>{concert.name}</h2>
+            <p>
+              {concert.date} - {concert.location}
+            </p>
+
+            <ul>
+              {concert.bands.map(band => (
+                <li key={band}>{band}</li>
+              ))}
+            </ul>
+          </ListItem>
+        ))}
+      </ol>
+    </PageLayout>
   );
 }
 
