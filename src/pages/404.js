e13ea66dcf23c9bcafdaf7bbaf0e83a3c109f6e3
Benjamim Sonntag <benjamimsonntag@gmail.com>
2019-11-03 20:34:02 +0000

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
 import { Metatags } from '../components/metatags';
+import { PageLayout } from '../components/page-layout';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+`;
 
 function NotFoundPage() {
   return (
-    <main>
+    <PageLayout>
       <Metatags />
-      <h1>NOT FOUND</h1>
-    </main>
+      <Wrapper>
+        <h1>NOT FOUND</h1>
+        <Link to='/'>Go home</Link>
+      </Wrapper>
+    </PageLayout>
   );
 }
 
